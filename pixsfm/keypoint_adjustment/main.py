9e3ecee2cd1366b342bc6af280a2b7f66ec64f67
Jonathan Gilat <you@example.com>
2025-03-17 17:43:52 +0000

    fixup! fixup! final test

--- pixsfm/keypoint_adjustment/main.py
+++ pixsfm/keypoint_adjustment/main.py
@@ -63,7 +63,7 @@ class KeypointAdjuster:
         'apply': True,
         'interpolation': base.interpolation_default_conf,
         'level_indices': None,
-        'max_kps_per_problem': 100,
+        'max_kps_per_problem': 150,
         'optimizer': {
             'loss': {
                 'name': 'cauchy',
@@ -72,7 +72,7 @@ class KeypointAdjuster:
             'solver': {
                 **base.solver_default_conf,
                 'parameter_tolerance': 1.0e-6,
-                'num_threads': -1
+                'num_threads': 1
             },
             'print_summary': False,
             'bound': 5.0,
